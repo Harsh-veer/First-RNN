242ba7b09213ced8e49cb1d205ee159dc817eb04
Harshveer <harshveer321.code@gmail.com>
2018-06-19 22:47:36 +0530

    separate functions for training and predicting

--- naiveRNN.py
+++ naiveRNN.py
@@ -24,8 +24,8 @@ for i in data:
 for i in X[1:]:
     Y.append(i)
 
-X=np.array(X[0:-1]) # 250
-Y=np.array(Y) # 250
+X=np.array(X[0:-1])
+Y=np.array(Y)
 
 def softmax(y):
     return np.exp(y)/np.sum(np.exp(y))
@@ -46,42 +46,59 @@ learning_rate=0.1
 mWxh, mWhh, mWhy = np.zeros_like(Wxh), np.zeros_like(Whh), np.zeros_like(Why)
 mbh, mby = np.zeros_like(bh), np.zeros_like(by)
 
-while True:
-    hs=np.zeros([hidden_size,1])
-    pre_hs=np.zeros([hidden_size,1])
-    dhsprev=np.zeros([hidden_size,1])
+def train():
+    loss=0
+    for _ in range(700):
+        hs=np.zeros([hidden_size,1])
+        pre_hs=np.zeros([hidden_size,1])
+        dhsprev=np.zeros([hidden_size,1])
 
+        # genstr=""
+
+        for k in range(len(X)):
+            x=np.array(np.matrix(X[k]).T)
+            pre_hs=np.copy(hs)
+            hs=np.tanh(np.dot(Wxh,x)+np.dot(Whh,pre_hs)+bh)
+            ys=np.dot(Why,hs)+by
+            ps=softmax(ys)
+
+            loss=-np.log(ps[list(Y[k]).index(1)])
+            print (loss)
+            # predicted=np.zeros([vocab_size,1])
+            # predicted[list(ps).index(max(ps))]=1
+            # genstr+=getkey(predicted,features)
+
+            dYs=np.copy(ps)
+            dYs[list(Y[k]).index(1)]-=1
+            dWhy=np.dot(dYs,hs.T)
+            dby=np.copy(dYs)
+            dhs=np.dot(Why.T,dYs)+dhsprev
+            dhsraw=(1-hs*hs)*dhs
+            dWxh=np.dot(dhsraw,x.T)
+            dbh=np.copy(dhsraw)
+            dWhh=np.dot(dhsraw,pre_hs.T)
+            dhsprev=np.dot(Whh.T,dhsraw)
+
+            for dparam in [dWxh, dWhh, dWhy, dbh, dby]:
+                np.clip(dparam, -5, 5, out=dparam)
+
+            for param, dparam, mem in zip([Wxh, Whh, Why, bh, by],[dWxh, dWhh, dWhy, dbh, dby],[mWxh, mWhh, mWhy, mbh, mby]):
+                mem += dparam * dparam
+                param += -learning_rate * dparam / np.sqrt(mem + 1e-8)
+
+
+def predict(beg,n): #starting char and number of chars to generategenstr=""
+    px=np.array(np.matrix(features[beg]).T)
+    ppre_hs=np.zeros([hidden_size,1])
     genstr=""
+    for _ in range(n):
+        phs=np.tanh(np.dot(Wxh,px)+np.dot(Whh,ppre_hs)+bh)
+        pys=np.dot(Why,phs)+by
+        pps=softmax(pys)
+        ppredicted=np.zeros([vocab_size,1])
+        ppredicted[list(pps).index(max(pps))]=1
+        genstr+=getkey(ppredicted,features)
+        px=np.copy(ppredicted)
+        ppre_hs=np.copy(phs)
 
-    for k in range(len(X)):
-        x=np.array(np.matrix(X[k]).T)
-        pre_hs=np.copy(hs)
-        hs=np.tanh(np.dot(Wxh,x)+np.dot(Whh,pre_hs)+bh)
-        ys=np.dot(Why,hs)+by
-        ps=softmax(ys)
-
-        loss=-np.log(ps[list(Y[k]).index(1)])
-        print (loss)
-        predicted=np.zeros([vocab_size,1])
-        predicted[list(ps).index(max(ps))]=1
-        genstr+=getkey(predicted,features)
-
-        dYs=np.copy(ps)
-        dYs[list(Y[k]).index(1)]-=1
-        dWhy=np.dot(dYs,hs.T)
-        dby=np.copy(dYs)
-        dhs=np.dot(Why.T,dYs)+dhsprev
-        dhsraw=(1-hs*hs)*dhs
-        dWxh=np.dot(dhsraw,x.T)
-        dbh=np.copy(dhsraw)
-        dWhh=np.dot(dhsraw,pre_hs.T)
-        dhsprev=np.dot(Whh.T,dhsraw)
-
-        for dparam in [dWxh, dWhh, dWhy, dbh, dby]:
-            np.clip(dparam, -5, 5, out=dparam)
-
-        for param, dparam, mem in zip([Wxh, Whh, Why, bh, by],[dWxh, dWhh, dWhy, dbh, dby],[mWxh, mWhh, mWhy, mbh, mby]):
-            mem += dparam * dparam
-            param += -learning_rate * dparam / np.sqrt(mem + 1e-8)
-
-    print (genstr)
+    return genstr
